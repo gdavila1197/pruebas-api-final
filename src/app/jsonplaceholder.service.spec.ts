b3abc9be5c9c624e2c4ea75a876935c145b3fef1
Gustavo de Avila <gustavodeavila@MacBook-Air-de-Gus.local>
2024-05-27 11:45:37 -0600

    prueba front

--- src/app/jsonplaceholder.service.spec.ts
+++ src/app/jsonplaceholder.service.spec.ts
@@ -31,7 +31,7 @@ describe('JsonPlaceholderService', () => {
 
     const request = httpMock.expectOne(`${service['baseURL']}/posts`);
     expect(request.request.method).toBe('GET');
-    request.flush(dummyPosts);
+    request.flush([]);
   });
 
   it('Obtener un post por id específico', () => {
